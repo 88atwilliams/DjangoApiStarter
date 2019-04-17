b9f218c728e3a739487e15b8e6381b65ef834495
[Alex <atwilliams88@gmail.com>
2019-04-17 10:49:23 -0500

    initial upload

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+
+# this would be api/v1/...
+urlpatterns = [
+    path('users/', include('users.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+]
